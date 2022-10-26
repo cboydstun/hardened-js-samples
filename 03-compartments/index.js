6463938ae391e97d924c4df5ab4ee1f336bb6567
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2022-10-26 11:10:54 -0500

    compartments init

--- 03-compartments/index.js
+++ 03-compartments/index.js
@@ -4,9 +4,24 @@ import 'ses';
 lockdown();
 
 const c = new Compartment({
-  print: harden(console.log),
+    print: harden(console.log),
 });
 
 c.evaluate(`
   print('Hello! Hello?');
 `);
+
+console.log(c.globalThis === globalThis); // false
+console.log(c.globalThis.JSON === JSON); // true
+
+const c1 = new Compartment();
+const c2 = new Compartment();
+console.log(c1.globalThis === c2.globalThis); // false
+console.log(c1.globalThis.JSON === c2.globalThis.JSON); // true
+
+const f1 = new c.globalThis.Function('return globalThis');
+f1() === c1.globalThis; // true
+const f2 = new c.globalThis.Function('return globalThis');
+f2() === c2.globalThis; // true
+
+f1() === f2(); // false
