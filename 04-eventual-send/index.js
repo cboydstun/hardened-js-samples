020dd2d0ea866a6cf6d79c92ca2986bedce0e6f8
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2022-10-26 11:58:54 -0500

    notes notes notes

--- 04-eventual-send/index.js
+++ 04-eventual-send/index.js
@@ -1,15 +1,25 @@
 // https://www.youtube.com/watch?v=UXR0O-CufTk
 // https://github.com/tc39/proposal-eventual-send
+// import { E } from '@agoric/far';
+// // Invoke pipelined RPCs.
+// const fileP = E(
+//   E(target).openDirectory(dirName)
+// ).openFile(fileName);
+// // Process the read results after a round trip.
+// E(fileP).read().then(contents => {
+//   console.log('file contents', contents);
+//   // We don't use the result of this send.
+//   E(fileP, { _oneway: true }).append('fire-and-forget');
+// });
 
-import { E } from '@agoric/far';
 
-// Invoke pipelined RPCs.
-const fileP = E(
-  E(target).openDirectory(dirName)
-).openFile(fileName);
-// Process the read results after a round trip.
-E(fileP).read().then(contents => {
-  console.log('file contents', contents);
-  // We don't use the result of this send.
-  E(fileP, { _oneway: true }).append('fire-and-forget');
-});
+// https://www.npmjs.com/package/@endo/captp
+// import { E, makeCapTP } from '@endo/captp';
+// // Create a message dispatcher and bootstrap.
+// // Messages on myconn are exchanged with JSON-able objects.
+// const { dispatch, getBootstrap, abort } = makeCapTP('myid', myconn.send, myBootstrap);
+// myconn.onReceive = obj => dispatch(obj);
+// // Get the remote's bootstrap object and call a remote method.
+// E(getBootstrap()).method(args).then(res => console.log('got res', res));
+// // Tear down the CapTP connection if it fails (e.g. connection is closed).
+// abort(Error('Connection aborted by user.'));
--- 06-remotables/index.js
+++ 06-remotables/index.js
@@ -1,14 +1,22 @@
 // https://docs.agoric.com/guides/js-programming/far.html#far-remotable-and-marshaling
+// import { Far, passStyleOf } from '@endo/marshal';
+// const makeCounter = () => {
+//     let count = 0;
+//     return Far('counter', {
+//         incr: () => (count += 1),
+//         decr: () => (count -= 1),
+//     });
+// };
+// const publicFacet = Far('makeCounter', { makeCounter });
+// assert(passStyleOf(publicFacet) === 'remotable');
 
-import { Far, passStyleOf } from '@endo/marshal';
+// https://www.npmjs.com/package/@endo/marshal
+// import '@endo/init';
+// import { makeMarshal } from '@endo/marshal';
+// const m = makeMarshal();
+// const o = harden({a: 1});
+// const s = m.serialize(o);
+// console.log(s); // { body: '{"a":1}', slots: [] }
+// const o2 = m.unserialize(s);
+// console.log(o2); // { a: 1 }
 
-const makeCounter = () => {
-    let count = 0;
-    return Far('counter', {
-        incr: () => (count += 1),
-        decr: () => (count -= 1),
-    });
-};
-
-const publicFacet = Far('makeCounter', { makeCounter });
-assert(passStyleOf(publicFacet) === 'remotable');
