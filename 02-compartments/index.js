a625acaf58b87f39dd166a243258f8c6034f063f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2022-10-26 09:10:10 -0500

    compartment refactor

--- 02-compartments/index.js
+++ 02-compartments/index.js
@@ -1,3 +1,8 @@
+//https://hackmd.io/@ZFlbCaR1Q06UzHk6tcn2dg/rk1yMNeQF#
+import "ses";
+
+lockdown();
+
 const makeCounter = init => {
     let value = init;
     return harden({
@@ -7,6 +12,23 @@ const makeCounter = init => {
     });
 };
 
+const c1 = makeCounter(1); c1.increment();
+const c2 = c1.makeOffsetCounter(10);
+c2.increment();
+[c1.increment(), c2.increment()];
+
+
+//pervasive mutability
+//gives increment unwanted properties
+c1.increment = () => console.log('launch the missiles!');
+//console log "launch the missiles!"
+c1.increment();
+
+
+//defensive objects
+//frozen object
 const c3 = Object.freeze(c1.makeOffsetCounter(10));
+//attempt to add property to frozen object
 c3.increment = () => { console.log('launch the missiles!'); }
+//returns correct value
 c3.increment()
